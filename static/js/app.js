0261e58bf28aa9475273d140c5e777fc668f0243
rubadhanaraj <rubadhanaraj@outlook.com>
2022-09-10 01:31:53 -0400

    Updating index.html

--- static/js/app.js
+++ static/js/app.js
@@ -4,14 +4,14 @@ const tableData = data;
 // Reference the HTML table using d3
 var tbody = d3.select("tbody");
 
+
 function buildTable(data) {
-    // First, clear out any existing data
+    //First, clear out any existing data
     tbody.html("");
-}
 
 // Next, loop through each object in the data
 // and append a row and cells for each value in the row
-data.forEach((dataRow) => {
+    data.forEach((dataRow) => {
     // Append a row to the table body
     let row = tbody.append("tr");
     // Loop through each field in the dataRow and add
@@ -22,6 +22,7 @@ data.forEach((dataRow) => {
     }
     );
 });
+}
 
 // filtering the table by date
 function handleClick() {
